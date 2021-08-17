baba9bca3ae30a09a42d4537a88ac9017b7ec846
nedalalashqar <nedal.r.alashqar@gmail.com>
2021-08-17 20:09:28 +0300

    add

--- starter-code/api-server/src/server.js
+++ starter-code/api-server/src/server.js
@@ -2,8 +2,8 @@
 
 const express = require('express');
 
-const errorHandler = require('./error-handlers/404.js');
-const {notFound} = require('./error-handlers/500.js');
+// const errorHandler = require('./error-handlers/404.js');
+// const {notFound} = require('./error-handlers/500.js');
 const logger = require('./middleware/logger.js');
 
 const v1Routes = require('./routes/v1');
@@ -16,8 +16,8 @@ app.use(logger);
 
 app.use( v1Routes);
 
-app.use(notFound);
-app.use(errorHandler);
+// app.use(notFound);
+// app.use(errorHandler);
 
 
 module.exports = {
